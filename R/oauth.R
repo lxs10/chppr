e2553dc85f9a206be567ec17f5ae88cfef798ef5
lxs <lxs@tutanota.com>
2018-09-19 21:40:05 +0200

    List OAuth URLs

--- R/oauth.R
+++ R/oauth.R
@@ -17,3 +17,15 @@ consumer <- function(key, secret, challenge = FALSE, order = FALSE,
 token <- function(token, secret) {
   structure(list(token = token, secret = secret), class = "token")
 }
+
+oauth_url <- function() {
+  list(
+    request_token = "https://chpp.hattrick.org/oauth/request_token.ashx",
+    authorize = "https://chpp.hattrick.org/oauth/authorize.aspx",
+    authenticate = "https://chpp.hattrick.org/oauth/authenticate.aspx",
+    access_token = "https://chpp.hattrick.org/oauth/access_token.ashx",
+    check_token = "https://chpp.hattrick.org/oauth/check_token.ashx",
+    invalidate_token = "https://chpp.hattrick.org/oauth/invalidate_token.ashx",
+    chppxml = "http://chpp.hattrick.org/chppxml.ashx"
+  )
+}
