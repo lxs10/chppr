f6d9e30c20bea1f6c2c66929c3aa05141855cc72
lxs <lxs@tutanota.com>
2018-09-21 23:22:23 +0200

    Get request token, authorization URL, access token, and CHPP XML

--- R/oauth.R
+++ R/oauth.R
@@ -52,7 +52,7 @@ oauth_encode <- function(x) {
   sapply(x, encode)
 }
 
-oauth_parameters <- function(url, consumer, callback, token, verifier) {
+oauth_parameters <- function(url, consumer, token, verifier, callback) {
   url <- split_url(url)
   parameters <- list(
     oauth_callback = callback,
@@ -75,9 +75,9 @@ oauth_parameters <- function(url, consumer, callback, token, verifier) {
   parameters[order(names(parameters))]
 }
 
-oauth_header <- function(url, consumer, callback = NULL, token = NULL,
-                         verifier = NULL) {
-  parameters <- oauth_parameters(url, consumer, callback, token, verifier)
+oauth_header <- function(url, consumer, token = NULL, verifier = NULL,
+                         callback = NULL) {
+  parameters <- oauth_parameters(url, consumer, token, verifier, callback)
   authorization <- paste0(
     "OAuth ",
     paste0(
@@ -86,4 +86,41 @@ oauth_header <- function(url, consumer, callback = NULL, token = NULL,
     )
   )
   httr::add_headers(Authorization = authorization)
+}
+
+request_token <- function(consumer, callback = "oob") {
+  url <- oauth_url()$request_token
+  response <- httr::GET(url, oauth_header(url, consumer, callback = callback))
+  content <- httr::content(response, "text", "text/html", "UTF-8")
+  token <- sapply(strsplit(unlist(strsplit(content, "&"))[1:2], "="), `[`, 2)
+  token(token[1], token[2])
+}
+
+authorization_url <- function(consumer, request_token) {
+  url <- paste0(oauth_url()$authorize, "?oauth_token=", request_token$token)
+  scope <- c(
+    "manage_challenges",
+    "set_matchorder",
+    "manage_youthplayers",
+    "set_training",
+    "place_bid"
+  )
+  scope <- scope[unlist(consumer[3:7])]
+  if (length(scope)) url <- paste0(url, "&scope=", paste(scope, collapse = ","))
+  url
+}
+
+access_token <- function(consumer, request_token, verifier) {
+  url <- oauth_url()$access_token
+  response <- httr::GET(url, oauth_header(url, consumer, request_token, verifier))
+  content <- httr::content(response, "text", "text/html", "UTF-8")
+  token <- sapply(strsplit(unlist(strsplit(content, "&")), "="), `[`, 2)
+  token(token[1], token[2])
+}
+
+chppxml <- function(query, consumer, access_token) {
+  url <- paste0(oauth_url()$chppxml, "?", query)
+  response <- httr::GET(url, oauth_header(url, consumer, access_token))
+  content <- httr::content(response, "text", "text/xml", "UTF-8")
+  content
 }
