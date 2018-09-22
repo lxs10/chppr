b455de67cda881f775cc3d41f90b336eb41ff744
lxs <lxs@tutanota.com>
2018-09-22 10:55:51 +0200

    Document consumer and token

--- R/oauth-constructors.R
+++ R/oauth-constructors.R
@@ -1,3 +1,30 @@
+#' OAuth Consumer
+#'
+#' Create a consumer.
+#'
+#' @param key Consumer key.
+#' @param secret Consumer secret.
+#' @param challenge Manage challenges?
+#' @param order Set match orders?
+#' @param youth Manage youth players?
+#' @param training Set training?
+#' @param bid Place bids?
+#'
+#' @return An object of class \code{"consumer"}.
+#' @export
+#'
+#' @examples
+#' # consumer key and secret below are for illustrative purposes only
+#' my_consumer <- consumer(
+#'   "WdGMxbWypO3baqGSkrZGtX",
+#'   "Bp7dG0GVFJNkAUPT6rHuptmlt1JRqJVIC46ElPHxejf"
+#' )
+#' # ask for permission to manage challenges
+#' my_consumer <- consumer(
+#'   "WdGMxbWypO3baqGSkrZGtX",
+#'   "Bp7dG0GVFJNkAUPT6rHuptmlt1JRqJVIC46ElPHxejf",
+#'   challenge = TRUE
+#' )
 consumer <- function(key, secret, challenge = FALSE, order = FALSE,
                      youth = FALSE, training = FALSE, bid = FALSE) {
   structure(
@@ -14,6 +41,7 @@ consumer <- function(key, secret, challenge = FALSE, order = FALSE,
   )
 }
 
+#' @export
 print.consumer <- function(x, ...) {
   cat("Consumer key:         ", x$key, "\n", sep = "")
   cat("Consumer secret:      ", x$secret, "\n", sep = "")
@@ -24,10 +52,24 @@ print.consumer <- function(x, ...) {
   cat("Place bids:           ", x$bid, "\n", sep = "")
 }
 
+#' OAuth Token
+#'
+#' Create a token.
+#'
+#' @param token Token.
+#' @param secret Token secret.
+#'
+#' @return An object of class \code{"token"}.
+#' @export
+#'
+#' @examples
+#' # token and token secret below are for illustrative purposes only
+#' my_token <- token("pCJS5wFvFfJWVhtY", "irhfsbr7C8c3OwtT")
 token <- function(token, secret) {
   structure(list(token = token, secret = secret), class = "token")
 }
 
+#' @export
 print.token <- function(x, ...) {
   cat("Token:        ", x$token, "\n", sep = "")
   cat("Token secret: ", x$secret, "\n", sep = "")
---
+++ data-raw/key-secret-example.R
@@ -0,0 +1,9 @@
+set.seed(0)
+
+# consumer key and secret
+paste(sample(sort(c(LETTERS, letters, 0:9)), 22, TRUE), collapse = "")
+paste(sample(sort(c(LETTERS, letters, 0:9)), 43, TRUE), collapse = "")
+
+# token and token secret
+paste(sample(sort(c(LETTERS, letters, 0:9)), 16, TRUE), collapse = "")
+paste(sample(sort(c(LETTERS, letters, 0:9)), 16, TRUE), collapse = "")
