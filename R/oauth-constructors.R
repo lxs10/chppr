fd83bc9b6b91cd6839886a4e5255ace8baef7e84
lxs <lxs@tutanota.com>
2018-09-22 10:32:13 +0200

    Print consumer and token

--- R/oauth-constructors.R
+++ R/oauth-constructors.R
@@ -14,6 +14,21 @@ consumer <- function(key, secret, challenge = FALSE, order = FALSE,
   )
 }
 
+print.consumer <- function(x, ...) {
+  cat("Consumer key:         ", x$key, "\n", sep = "")
+  cat("Consumer secret:      ", x$secret, "\n", sep = "")
+  cat("Manage challenges:    ", x$challenge, "\n", sep = "")
+  cat("Set match orders:     ", x$order, "\n", sep = "")
+  cat("Manage youth players: ", x$youth, "\n", sep = "")
+  cat("Set training:         ", x$training, "\n", sep = "")
+  cat("Place bids:           ", x$bid, "\n", sep = "")
+}
+
 token <- function(token, secret) {
   structure(list(token = token, secret = secret), class = "token")
+}
+
+print.token <- function(x, ...) {
+  cat("Token:        ", x$token, "\n", sep = "")
+  cat("Token secret: ", x$secret, "\n", sep = "")
 }
